e3526059c4b2d7d686ba971fad135b352d0b1c8a
000462312 <000462312@CC147XX1A012XXX>
2023-05-06 11:18:34 -0700

    TestReadLine

---
+++ 1303Day2/1303Day2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1303Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.TestReadLine();
+        }
+
+        public void TestReadLine()
+        {
+            Console.WriteLine("Enter one or more lines - press CTRL + Z to exit: ");
+            string line;
+
+            do
+            {
+                Console.Write(" ==> ");
+                line = Console.ReadLine();
+                if (line != null)
+                {
+                    Console.WriteLine($"{line}");
+                }
+
+            } while (line != null);
+        }
+    }
+}
