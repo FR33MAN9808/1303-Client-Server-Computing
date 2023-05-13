b09f06f02e772d9dc648848c8aa7029168388a22
000462312 <000462312@CC145XX1A017XXX>
2023-05-12 19:55:26 -0700

    practice

---
+++ Day3/practice/practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using practice;
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Program program = new Program();
+
+        var userArray = new User[10];
+        for (int i = 0; i < userArray.Length; i++) 
+        {
+            userArray[i] = new User()
+            {
+                id = (i + 1),
+                name = "placeholder" + (i + 1)
+            };
+        }
+
+        program.printArray(userArray);
+    }
+
+    public void printArray(User[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"id:{arr[i].id}\nname:{arr[i].name}\n");
+        }
+    }
+}
---
+++ Day3/practice/practice/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class User
+    {
+        public int id;
+        public string name;
+    }
+}
