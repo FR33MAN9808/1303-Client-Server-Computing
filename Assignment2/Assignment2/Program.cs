0e63c94283ef620b66c947fb05c2ae3fb79c0abc
Chuanche <s824132394@gmail.com>
2023-05-16 19:46:19 -0700

    chanegd

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -2,16 +2,45 @@
 {
     private static void Main(string[] args)
     {
-        Program p = new Program();
+        //Program p = new Program();
 
-        Console.WriteLine("1 - Add user\r\n2 - Show user by ID\r\n3 - Show user by Name\r\n4 - Delete user by ID\r\n5 - Delete user by Name\r\n6 - List all users");
+        Console.WriteLine("1 - Add user\r\n2 - Show user by ID\r\n3 - Show user by Name\r\n4 - Delete user by ID\r\n5 - Delete user by Name\r\n6 - List all users\r\n7 - Exit");
 
+        int userInput = int.Parse(Console.ReadLine());
+
+        switch (userInput)
+        {
+            case 1:
+                //Add user
+                Console.WriteLine("");
+                User newUser = new User();
+                break;
+
+            case 2:
+            //Show user by ID
+
+            case 3:
+            //Show user by Name
+
+            case 4:
+            //Delete user by ID
+
+            case 5:
+            //Delete user by Name
+
+            case 6:
+            //List all users
+
+            case 7:
+                //Exit
+                break;
+        }
 
     }
 }
 
 class User
 {
-    public int id;
-    public string name;
+    public int id { get; set; }
+    public string name { get; set; }
 }
