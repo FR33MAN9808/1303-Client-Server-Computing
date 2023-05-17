097e39bf5d910e50b59c9f2b8792f3776d203826
Chuanche <s824132394@gmail.com>
2023-05-16 19:06:56 -0700

    hi

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Program p = new Program();
+
+        Console.WriteLine("1 - Add user\r\n2 - Show user by ID\r\n3 - Show user by Name\r\n4 - Delete user by ID\r\n5 - Delete user by Name\r\n6 - List all users");
+
+
+    }
+}
+
+class User
+{
+    public int id;
+    public string name;
+}
