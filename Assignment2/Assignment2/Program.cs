5cd046dc33c36ba3ba3284557797b6868a8a94e2
Chuanche <s824132394@gmail.com>
2023-05-16 20:10:57 -0700

    V1

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -1,46 +1,161 @@
-﻿internal class Program
+﻿using System;
+using System.Xml.Linq;
+
+internal class Program
 {
+    private static User[] users = new User[100];
+
     private static void Main(string[] args)
     {
-        //Program p = new Program();
+        int choice;
 
-        Console.WriteLine("1 - Add user\r\n2 - Show user by ID\r\n3 - Show user by Name\r\n4 - Delete user by ID\r\n5 - Delete user by Name\r\n6 - List all users\r\n7 - Exit");
-
-        int userInput = int.Parse(Console.ReadLine());
-
-        switch (userInput)
+        do
         {
-            case 1:
-                //Add user
-                Console.WriteLine("");
-                User newUser = new User();
-                break;
+            Console.WriteLine("Please choose an option:");
+            Console.WriteLine("1. Add user");
+            Console.WriteLine("2. Show user by ID");
+            Console.WriteLine("3. Show user by Name");
+            Console.WriteLine("4. Delete user by ID");
+            Console.WriteLine("5. Delete user by Name");
+            Console.WriteLine("6. List all users");
+            Console.WriteLine("0. Exit");
+            choice = int.Parse(Console.ReadLine());
 
-            case 2:
-            //Show user by ID
+            switch (choice)
+            {
+                case 1:
+                    AddUser();
+                    break;
+                case 2:
+                    ShowUserByID();
+                    break;
+                case 3:
+                    ShowUserByName();
+                    break;
+                case 4:
+                    DeleteUserByID();
+                    break;
+                case 5:
+                    DeleteUserByName();
+                    break;
+                case 6:
+                    ListAllUsers();
+                    break;
+                case 0:
+                    Console.WriteLine("Exiting program...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
 
-            case 3:
-            //Show user by Name
+        } while (choice != 0);
+    }
 
-            case 4:
-            //Delete user by ID
+    private static void AddUser()
+    {
+        Console.WriteLine("Enter user ID:");
+        int id = int.Parse(Console.ReadLine());
 
-            case 5:
-            //Delete user by Name
+        Console.WriteLine("Enter user name:");
+        string name = Console.ReadLine();
 
-            case 6:
-            //List all users
+        User user = new User();
+        user.ID = id;
+        user.Name = name;
 
-            case 7:
-                //Exit
-                break;
+        users[id] = user;
+    }
+
+    private static void ShowUserByID()
+    {
+        Console.WriteLine("Enter user ID:");
+        int id = int.Parse(Console.ReadLine());
+
+        if (users[id] != null)
+        {
+            Console.WriteLine($"User ID: {users[id].ID}, Name: {users[id].Name}");
+        }
+        else
+        {
+            Console.WriteLine("User not found.");
+        }
+    }
+
+    private static void ShowUserByName()
+    {
+        Console.WriteLine("Enter user name:");
+        string name = Console.ReadLine();
+
+        bool userFound = false;
+
+        for (int i = 0; i < users.Length; i++)
+        {
+            if (users[i] != null && users[i].Name == name)
+            {
+                Console.WriteLine($"User ID: {users[i].ID}, Name: {users[i].Name}");
+                userFound = true;
+            }
         }
 
+        if (userFound == false)
+        {
+            Console.WriteLine("User not found.");
+        }
+    }
+
+    private static void DeleteUserByID()
+    {
+        Console.WriteLine("Enter user ID:");
+        int id = int.Parse(Console.ReadLine());
+        if (users[id] != null)
+        {
+            users[id] = null;
+            Console.WriteLine("User deleted.");
+        }
+        else
+        {
+            Console.WriteLine("User doesn't exist");
+        }
+    }
+
+    private static void DeleteUserByName()
+    {
+        Console.WriteLine("Enter user Name:");
+        string name = Console.ReadLine();
+
+        bool userFound = false;
+
+        for (int i = 0; i < users.Length; i++)
+        {
+            if (users[i] != null && users[i].Name == name)
+            {
+                users[i] = null;
+                Console.WriteLine("User deleted.");
+                userFound = true;
+            }
+        }
+
+        if (userFound == false)
+        {
+            Console.WriteLine("User not found.");
+        }
+    }
+
+    private static void ListAllUsers()
+    {
+        for (int i = 0; i < users.Length; i++)
+        {
+            if (users[i] != null)
+            {
+                Console.WriteLine($"User ID: {users[i].ID}, Name: {users[i].Name}");
+            }
+        }
     }
 }
 
 class User
 {
-    public int id { get; set; }
-    public string name { get; set; }
+    public int ID;
+    public string Name;
 }
