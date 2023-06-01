93b1fc000c2a3bb060863a75c54da93c0b0bf9df
Chuanche <s824132394@gmail.com>
2023-06-01 12:51:53 -0700

    assignment 4 done

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Assignment4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Car car = new Car();
+        car.Brand = "Ford";
+        Console.WriteLine(car.Brand);
+    }
+
+    public abstract class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int NumberOfWheels { get; set; }
+        public EngineType EngineType { get; set; }
+        public int Speed { get; set; }
+        public int MaximumSpeed { get; set; }
+    }
+
+    public enum EngineType
+    {
+        Electric,
+        Combustion
+    }
+
+    public class Car : Vehicle
+    {
+        public string BodyType { get; set; }
+        public string DashboardMileage { get; set; }
+        public string VIN { get; set; }
+        public string LicenseNumber { get; set; }
+        public int NumberOfDoors { get; set; }
+    }
+
+    public class Truck : Vehicle
+    {
+        public string BodyType { get; set; }
+        public int LoadCapacity { get; set; }
+    }
+
+    public class Bus : Vehicle
+    {
+        public int SeatingCapacity { get; set; }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public bool HasSideCar { get; set; }
+    }
+
+}
