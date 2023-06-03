e2d65e526d356fcb92b49867ae4bb3a2c25fc2fb
Chuanche <s824132394@gmail.com>
2023-06-03 10:27:53 -0700

    lab4 completed

---
+++ week5/Lab4/ClntSrvrWk1/Lessons/Week4/ObjectType/MyStack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClntSrvrWk1.Lessons.Week4.ObjectType
+{
+    public class MyStack
+    {
+        private int size;
+        private int position;
+        private object[] items;
+
+        public MyStack(int stackSize)
+        {
+            size = stackSize;
+            items = new object[size];
+            position = -1;
+        }
+
+        public void Push(object obj)
+        {
+            if (position == size - 1)
+            {
+                throw new InvalidOperationException("Stack overflow!");
+            }
+            items[++position] = obj;
+        }
+
+        public object Pop()
+        {
+            if (position == -1)
+            {
+                throw new InvalidOperationException("Stack underflow!");
+            }
+            return items[position--];
+        }
+
+        public object Peak()
+        {
+            if (position == -1)
+            {
+                throw new InvalidOperationException("Stack is empty!");
+            }
+            return items[position];
+        }
+    }
+}
