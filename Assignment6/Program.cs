effdfb06eba31b6db944ea70f751a79c9d6a2756
Chuanche <s824132394@gmail.com>
2023-06-14 20:09:58 -0700

    A6

---
+++ Assignment6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Assignment6;
+
+class Program
+{
+    delegate void DisplayDelegate(User user);
+
+    static void Main(string[] args)
+    {
+        List<User> users = new List<User>
+        {
+            new RegularUser { Name = "John Doe", Username = "johndoe", Email = "johndoe@example.com" },
+            new RegularUser { Name = "Jane Smith", Username = "janesmith", Email = "janesmith@example.com" },
+            new RegularUser { Name = "Mike Johnson", Username = "mikejohnson", Email = "mikejohnson@example.com" },
+            new RegularUser { Name = "Sarah Adams", Username = "sarahadams", Email = "sarahadams@example.com" },
+            new RegularUser { Name = "David Brown", Username = "davidbrown", Email = "davidbrown@example.com" },
+            new EliteUser { Name = "Emily Davis", Username = "emilydavis", Email = "emilydavis@example.com" },
+            new EliteUser { Name = "Michael Clark", Username = "michaelclark", Email = "michaelclark@example.com" },
+            new EliteUser { Name = "Olivia Evans", Username = "oliviaevans", Email = "oliviaevans@example.com" },
+            new EliteUser { Name = "James Green", Username = "jamesgreen", Email = "jamesgreen@example.com" },
+            new EliteUser { Name = "Sophia Harris", Username = "sophiaharris", Email = "sophiaharris@example.com" }
+        };
+
+        Display(users, DisplayUser);
+    }
+
+    static void DisplayUser(User user)
+    {
+        Console.WriteLine($"Name: {user.Name}");
+        Console.WriteLine($"Username: {user.Username}");
+        Console.WriteLine($"Email: {user.Email}");
+        Console.WriteLine();
+    }
+
+    static void Display(List<User> users, DisplayDelegate displayDelegate)
+    {
+        foreach (User user in users)
+        {
+            displayDelegate(user);
+        }
+    }
+}
---
+++ Assignment6/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+
+    public class RegularUser : User { }
+    public class EliteUser : User { }
+}
